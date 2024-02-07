03503eb2f055ac3f2465b2a7958147cfbbbab384
jtierney06 <jamespt06@icloud.com>
2024-02-07 14:45:37 -0500

    Create Page9.java

---
+++ Page9.java
@@ -0,0 +1,16 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: James Tierney
+*Date: Febuary 7th 2024
+*Program Name: Page9.java
+*************************************/
+
+import java.io.*;
+public class Page9
+{
+public static void main (String[ ] args)
+{
+System.out.println("Hi"); //message to user
+System.out.println("LabOneA"); //assignment
+}
+}
